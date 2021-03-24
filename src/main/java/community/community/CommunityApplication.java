a9b3289e6e8e2b5445f4458902586c30f98dcb34
mumblecoder <mumblecoder@gmail.com>
2021-03-24 22:59:19 +0900

    chapter 4 base

--- src/main/java/community/community/CommunityApplication.java
+++ src/main/java/community/community/CommunityApplication.java
@@ -5,17 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class CommunityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
-	}
-
-	@GetMapping
-	public String HelloWorld() {
-		return "Hello World";
 	}
 
 }
