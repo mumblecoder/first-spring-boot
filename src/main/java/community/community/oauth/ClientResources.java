a4c087f41126b29cbe8d6ba64f08e97831f084d3
mumblecoder <mumblecoder@gmail.com>
2021-04-11 00:10:32 +0900

    oauth2 에러로 인해 gradle 수정

---
+++ src/main/java/community/community/oauth/ClientResources.java
@@ -0,0 +1,14 @@
+package community.community.oauth;
+
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+public class ClientResources {
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+
+
+
+}
