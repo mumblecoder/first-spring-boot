f78bf05ce5d3679f2de30f028ed16bea0d2aa6ad
mumblecoder <mumblecoder@gmail.com>
2021-04-11 22:32:06 +0900

    oauth, social login 관련 설정파일 추가

--- src/main/java/community/community/oauth/ClientResources.java
+++ src/main/java/community/community/oauth/ClientResources.java
@@ -1,5 +1,6 @@
 package community.community.oauth;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
@@ -8,7 +9,15 @@ public class ClientResources {
     @NestedConfigurationProperty
     private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
 
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
 
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
 
+    public ResourceServerProperties getResource() {
+        return resource;
+    }
 
 }
